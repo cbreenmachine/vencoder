169c2a54435ee2dd589e4fbaeef76c43325134d6
Coleman Breen <ceb@cebs-m1.local>
2023-12-05 17:25:04 -0600

    setup.py

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+setup(
+    name='vencoder',
+    version='0.0.1',    
+    description='A sequence variant encoder',
+    url='https://github.com/cbreenmachine/vencoder',
+    author='Coleman Breen',
+    author_email='cebreen@wisc.edu',
+    license='MIT',
+    packages=['vencoder'],
+    install_requires=[],
+
+    classifiers=[
+        'Intended Audience :: Science/Research',
+        'Operating System :: POSIX :: Linux',        
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+)
